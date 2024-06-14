f53d832f24a9300a76d2acd4ae88c933c7a28cc4
justinnebaltazar <jfb3@sfu.ca>
2024-06-14 14:37:20 -0700

    Complete function to pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringLength = string.length - 1; 
+    let reversedString = '';
+    for(let i = stringLength; i >= 0; i--) {
+        reversedString = reversedString + string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
