94675a6ea9e2b0694bb3372b7463475f4c528eb7
justinnebaltazar <jfb3@sfu.ca>
2024-06-14 15:20:37 -0700

    Complete removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArr, values ) {
+    let finalArr = [];
+    for (num in originalArr) {
+        if (values.includes(num)) {
+            continue
+        }
+        finalArr.push(num);
+    }
+    return finalArr;
 };
 
 // Do not edit below this line
