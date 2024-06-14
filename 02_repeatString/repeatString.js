2c2ff4d8c474ecb64c4784ed4dd98269fb79e5c0
justinnebaltazar <jfb3@sfu.ca>
2024-06-14 14:08:30 -0700

    Finish repeatString function to pass the first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = '';
+    for(let i = 0; i < num; i++) {
+        finalString = finalString + string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
