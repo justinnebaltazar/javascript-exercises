e73acbc794ce37d1208228b9ce8c09027698ac30
justinnebaltazar <jfb3@sfu.ca>
2024-06-14 23:40:17 -0700

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    let typeOfBegin = typeof begin;
+    let typeOfEnd = typeof end;
 
+    if (begin < 0 || end < 0 || typeOfBegin != 'number'|| typeOfEnd !=  'number') {
+        return 'ERROR';
+    }
+    
+    let sum = 0; 
+    if (begin > end) {
+        let temp = begin;
+        begin = end; 
+        end = temp;
+    }
+
+    for (let i = begin; i < (end + 1); i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
