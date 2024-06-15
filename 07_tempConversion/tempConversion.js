b7f082d64afca326ca9b716bdd563654db4b84f5
justinnebaltazar <jfb3@sfu.ca>
2024-06-15 00:13:58 -0700

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+
+  let tempInCelcius  = (tempInFarenheit - 32) * (5/9);
+  let rounded = Math.round(tempInCelcius * 10) / 10;
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+
+  let tempInFarenheit = (tempInCelcius * (9/5)) + 32;
+  let rounded = Math.round(tempInFarenheit * 10) / 10;
+  return rounded;
+
 };
 
 // Do not edit below this line
