4f6e7bc02c4003e60f30288e86b4e41a3a3ac757
justinnebaltazar <jfb3@sfu.ca>
2024-06-23 21:36:43 -0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -21,8 +21,14 @@ const power = function(a, b) {
     return a ** b; 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  
+  if (a === 0) return 1; 
+  let product = 1; 
+  for(let i = a; i > 0; i--) {
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line
