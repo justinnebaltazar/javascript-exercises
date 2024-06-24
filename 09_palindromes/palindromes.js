6bb5f2704ad9b11546d22b6ed23114276ecbe135
justinnebaltazar <jfb3@sfu.ca>
2024-06-24 11:30:08 -0700

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,13 @@
 const palindromes = function (str) {
-
-      // Since we only consider letters and numbers, create a variable containing all valid characters
-  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
-  const cleanedString = str
-    .toLowerCase()
-    .split('')
-    .filter((character) => alphanumerical.includes(character))
-    .join('');
-
-  // Create a new reversed string for comparison
+  const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let cleanedString = str
+      .toLowerCase()
+      .split('')
+      .filter((character) => alphanumeric.includes(character))
+      .join('');
+  
   const reversedString = cleanedString.split('').reverse().join('');
-
-  // Return the outcome of the comparison which will either be true or false
   return reversedString === cleanedString;
 };
+
+module.exports = palindromes;
