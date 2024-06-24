5ad2eed132f5c49d7a9bb87496a8676d3ca220bd
justinnebaltazar <jfb3@sfu.ca>
2024-06-24 16:38:42 -0700

    finish exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function(yearOfBirth, yearOfDeath) {
+    if (!yearOfDeath) {
+        yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+    const oldest = arr.sort(function(a, b) {
+        const lastPerson = getAge(a.yearOfBirth, a.yearOfDeath);
+        const nextPerson = getAge(b.yearOfBirth, b.yearOfDeath);
+        return lastPerson < nextPerson ? 1 : -1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
