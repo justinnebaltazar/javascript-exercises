12f7606bb20407a1700d5bfb18b5bb7bcdd50250
justinnebaltazar <jfb3@sfu.ca>
2024-06-24 16:39:14 -0700

    finish exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
+    let num;
+    if (typeof numArg !== 'number') {
+        num = parseInt(numArg);
+    } else {
+        num = numArg;
+    }
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
 
+    const fib = [0, 1]; 
+    for(let i = 2; i <= num; i++) {
+        fib[i] = fib[i-2] + fib[i-1]
+    }
+    return fib[num];
 };
 
 // Do not edit below this line
